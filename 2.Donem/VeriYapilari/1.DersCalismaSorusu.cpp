d55e6442fc849e67b52f0df9e82f96fe4bc431e3
OyunErbabi <mehmet1992@hotmail.com>
2025-02-11 20:17:00 +0300

    Calisma sorusu düzenlendi

--- 2.Donem/VeriYapilari/1.DersCalismaSorusu.cpp
+++ 2.Donem/VeriYapilari/1.DersCalismaSorusu.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 using namespace std;
 
@@ -26,5 +27,60 @@ int main()
 	cout<<"Sayi 2:"<<endl;
 	cin>>b;
 	cout<<"Sonuc : "<<recursiveMultiply(a,b);
+	return 0;
+}
+*/
+
+///////////////////////////////////////////////
+/*
+Ornek Soru
+
+Soru:
+Bir ciftlikte, her tavuk her gun x tane yumurta yumurtlamaktadir.
+n gun boyunca toplam kac yumurta uretilecegini ozyinelemeli (recursive) bir fonksiyon kullanarak hesaplayan bir C++ programi yazin.
+
+Kurallar:
+
+-Ozyineleme (recursion) kullanmalisiniz.
+-Carpma (*) islemi kullanilmamalidir.
+-Sadece toplama (+) islemi kullanilmalidir.
+*/
+
+#include <iostream>
+using namespace std;
+
+int ToplamYumurta = 0;
+
+int carpma(int a, int b) {
+    if (b == 0) return 0;
+    return a + carpma(a, b - 1);
+}
+
+int RecursiveDays(int tavuk,int yumurta,int gun)
+{
+	if(gun==0)
+	{
+		return 0;
+	}
+	else if(gun==1)
+	{
+		return tavuk * yumurta;	
+	}
+	else 
+	{
+		return carpma(tavuk,yumurta) + RecursiveDays(tavuk,yumurta,gun-1);
+	}
+}
+
+int main()
+{
+	int t,y,g;
+	cout<<"Tavuk Adeti:"<<endl;
+	cin>>t;
+	cout<<"Gunde Ne Kadar Yumurtluyor :"<<endl;
+	cin>>y;
+	cout<<"Kac Gun:"<<endl;
+	cin>>g;
+	cout<<"Toplam Yumurta Sayisi: "<<RecursiveDays(t,y,g);
 	return 0;
 }
