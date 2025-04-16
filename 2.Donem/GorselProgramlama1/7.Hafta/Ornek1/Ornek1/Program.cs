7724e7bbe852ec7f6f99554da05ff7201f3a14f2
Mehmet Akif ERSOY <mehmet1992@hotmail.com>
2025-04-16 18:21:53 +0300

    Web programlama dersi işlendi

---
+++ 2.Donem/GorselProgramlama1/7.Hafta/Ornek1/Ornek1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a, b,c;
+            a = "Bilgisayar";
+            b="Programcılığı";
+            Console.WriteLine(b);
+            
+
+            c=String.Concat(a," ",b); // iki stringi birleştirir
+            Console.WriteLine(c);
+
+            b=string.Empty; // stringi boşaltır
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
